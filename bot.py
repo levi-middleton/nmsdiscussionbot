5ae5bff8c9070f7aa9558fe1693badda0e0f72f0
Levi Middleton <levi@theawesomefiles.com>
2018-08-16 07:23:13 -0400

    Added auto-approval

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ conn = sqlite3.connect('db.sqlite')
 conn.execute('CREATE TABLE IF NOT EXISTS submissions (name text NOT NULL UNIQUE, title text NOT NULL, hot integer NOT NULL)')
 c = conn.cursor()
 r = praw.Reddit('bot1')
-print(r.user.me())
+print(r.user.me())	
 c.execute('UPDATE submissions SET hot = 0')
 for submission in r.subreddit('nomansskythegame').hot(limit=1000):
 	if hasattr(submission,'link_flair_text') and (('Information' in str(submission.link_flair_text)) or ('Discussion' in str(submission.link_flair_text))):
@@ -15,11 +15,12 @@ for submission in r.subreddit('nomansskythegame').hot(limit=1000):
 		if(number_of_rows==0):
 			new_submission = submission.crosspost(subreddit="NMS_Discussions",send_replies=False)
 			c.execute('INSERT INTO submissions(name,title,hot) VALUES (?,?,1)', (str(submission.name),str(submission.title)))
-			print('NEW: ' + str(submission.title))
 			new_submission.mod.lock()
+			if(not hasattr(submission,'post_hint')):
+				new_submission.mod.approve()
+			print('NEW: ' + str(submission.title))
 		else:
 			c.execute('UPDATE submissions SET hot = 1 WHERE name = ?',(str(submission.name),))
-			print('UPDATE: ' + str(submission.title))
 
 c.execute('DELETE FROM submissions WHERE hot = 0')
 print('Deleted ' + str(c.rowcount) + ' rows.')
